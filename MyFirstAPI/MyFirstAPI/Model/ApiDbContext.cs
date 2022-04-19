b8884b8809bdaf7fc39844b4c14f4d459176617e
dev-assuncao <davisson.assuncao@protonmail.com>
2022-04-18 22:23:38 -0300

    Initializing Api with Db connection, model and controller of Provider

---
+++ MyFirstAPI/MyFirstAPI/Model/ApiDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstAPI.Model
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base (options)
+        {
+        }
+
+        public DbSet<Provider> Providers { get; set; }
+    }
+}
---
+++ MyFirstAPI/MyFirstAPI/Model/Provider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstAPI.Model
+{
+    public class Provider
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "The fild {0} is required")]
+        [StringLength(100, ErrorMessage = "The field {0} need has between {2} and {1} caracters", MinimumLength = 2)]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(14, ErrorMessage ="The field {0} need has between {2} and {1} caracters", MinimumLength = 2 )]
+        public string Document { get; set; }
+
+        public int TypeProvider { get; set; }
+        public bool Activity { get; set; }
+
+    }
+}
